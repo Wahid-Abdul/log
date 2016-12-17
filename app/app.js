22e3c2f6bf7a66513d38a604bce775b87a31bc19
akash227 <akash227@yahoo.com>
2016-12-18 02:52:38 +0530

    Mods #00

--- app/app.js
+++ app/app.js
@@ -2,22 +2,25 @@
 
 // Declare app level module which depends on views, and components
 angular.module('iFeed', [
-  'ui.router',
-  'iFeed.login',
-  'iFeed.signup',
-  'iFeed.home',
+    'ui.router',
+    'iFeed.login',
+    'iFeed.signup',
+    'iFeed.home',
     'iFeed.main',
-  'myApp.version'
+    'myApp.version'
 ])
-.config(function ($urlRouterProvider, $stateProvider, $locationProvider) {
-    // $locationProvider.html5Mode(true);
+    .controller('NavCtrl', function ($rootScope) {
+        $rootScope.nav = false;
+    })
+    .config(function ($urlRouterProvider, $stateProvider, $locationProvider) {
+        // $locationProvider.html5Mode(true);
 
-    $urlRouterProvider.otherwise("/home");
-})
-    .service("userService", function($state) {
+        $urlRouterProvider.otherwise("/home");
+    })
+    .service("userService", function ($state) {
         let $this = this;
-      this.user = {};
-        firebase.auth().onAuthStateChanged(function(user) {
+        this.user = {};
+        firebase.auth().onAuthStateChanged(function (user) {
             if (user) {
                 $this.user = user;
                 $state.go('main');
--- app/main/main.js
+++ app/main/main.js
@@ -14,16 +14,39 @@ angular.module('iFeed.main', ['ui.router'])
             })
     })
 
-    .controller('MainCtrl', ['userService', '$scope', function (userService, $scope) {
+    .controller('MainCtrl', ['userService', '$rootScope', '$scope','$http', function (userService, $rootScope, $scope,$http) {
         $(document).ready(function () {
             $(".button-collapse").sideNav();
             $('.collapsible').collapsible();
-        })
-        $scope.foods = [{
-            name: "Idly",
-            quantity: "10",
-            time: "10 pm",
-            place: "Sathyam Cinemas"
-
-        }]
+            $('select').material_select();
+        });
+        $scope.foods = [];
+        $scope.collect = function(){
+          
+        }
+        $scope.sortOpts = ['title', 'web_address', 'tag'];
+        $scope.fetch = function(){
+          console.log("frek it!!!")
+          function getLocation() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(showPosition);
+            } else {
+              x.innerHTML = "Geolocation is not supported by this browser.";
+            }
+          }
+          function showPosition(position) {
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+            console.log(position.coords.latitude);
+            // var latitude = 13.0564801;
+            // var longitude = 80.2592885;
+            var radius = parseFloat($scope.locate);
+            $http.post("http://localhost:3000/fetchFeed", {latitude:latitude,longitude:longitude,radius:radius}).success(function (data) {
+              console.log(data);
+              $scope.foods = data;
+            });
+          }
+          getLocation();
+        }
+        $rootScope.nav = true;
     }]);
